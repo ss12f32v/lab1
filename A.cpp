247040cc4de3b03a16698190d7ce37a425e11b69
ss12f32v <cloudstrife60138@gmail.com>
2015-03-17 01:20:48 +0800

    lab1 B code

--- A.cpp
+++ A.cpp
@@ -1,18 +1,17 @@
-＃include<iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
 int n;
-cout << "Enter a number:"
+cout << "Enter a number:";
 cin >> n;
 cout << n;
 while(n!=1)
 { 
  if (n%2 == 1) n = 3*n+1;
  else n =  n/2;
-cout << n	
+cout << n;	
 }
-system("pause");
 return 0;
 
 
